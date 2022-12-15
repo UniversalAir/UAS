13fd8356f78c630ca20221854720382c0e7cfd91
Narendra <npc@warlocktechnologies.com>
2022-12-15 12:40:17 +0530

    [IMP]add condition on replace url

--- website_universal/static/src/js/other_pricelist_price.js
+++ website_universal/static/src/js/other_pricelist_price.js
@@ -11,20 +11,22 @@ publicWidget.registry.otherPricelistPrice = publicWidget.Widget.extend({
 	_compute_price: function(ev){
 		var current_url = window.location.href;
 		var myArray = current_url.split("#");
-		var mtAttrs = myArray[1].replace('attr=','');
-		if (mtAttrs){
-			this._rpc({
-				route: '/product/price',
-				params:{
-					'mtAttrs': mtAttrs,	
-					'product_id' : parseInt(this.el.getElementsByClassName('product_template_id').product_template_id.value),
-					'add_qty':parseInt(this.el.getElementsByClassName('quantity').add_qty.value),
-				}
-			}).then(function(result){
-				if (result.price){
-					$('.other_oe_price')[0].getElementsByClassName('oe_currency_value')[0].innerText = parseFloat(result.price).toFixed(2)
-				}
-			});
+		if (myArray[1]){
+			var mtAttrs = myArray[1].replace('attr=','');
+			if (mtAttrs){
+				this._rpc({
+					route: '/product/price',
+					params:{
+						'mtAttrs': mtAttrs,	
+						'product_id' : parseInt(this.el.getElementsByClassName('product_template_id').product_template_id.value),
+						'add_qty':parseInt(this.el.getElementsByClassName('quantity').add_qty.value),
+					}
+				}).then(function(result){
+					if (result.price){
+						$('.other_oe_price')[0].getElementsByClassName('oe_currency_value')[0].innerText = parseFloat(result.price).toFixed(2)
+					}
+				});
+			}
 		}
 	}
 });
